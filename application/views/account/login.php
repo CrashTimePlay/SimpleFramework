b95529ebe0a5039e553cb5fd60682ffaea16c6f4
CrashTimePlay <abusecrash@gmail.com>
2022-07-24 21:40:42 +0600

    added core files

---
+++ application/views/account/login.php
@@ -0,0 +1,9 @@
+<h3>Вход</h3>
+<form>
+    <p>Логин</p>
+    <p><input type="text"></p>
+    <p>Пароль</p>
+    <p><input type="text"></p>
+    <b><button>Вход</button></b>
+    <p class="input" type="text"></p>
+</form>
